45286669f242da99ae4717ca19becabec6fac42c
Alunos <alunos@pedreira-local.org>
2024-11-19 17:41:50 -0300

    mudanças nos views e create

--- ConnecMazeDb/view.js
+++ ConnecMazeDb/view.js
@@ -1,4 +1,4 @@
-function connect(){
+function view(esc){
     const { Module } = require('module');
   
     const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -20,25 +20,27 @@ function connect(){
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         const db = client.db("Maze"); // banco de dados
-  
+
         const usuarios = db.collection("usuarios");
         const denuncias = db.collection("denuncias");
 
-        const teste = await denuncias.find();
+        if(esc == 'denuns'){
+        const teste = await denuncias.find({lat:1111.111},{projection:{ _id:0}}).toArray();
         console.log(teste);
-
+        }else{
+          const teste = await usuarios.find({},{projection:{ _id:0}}).toArray();
+          console.log(teste);
+        }
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
       } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
       }  
     }
-    
-
-  
-
     run().catch(console.dir);
   }
   
-  module.exports = connect;
-  connect();
+  module.exports = view;
+  
+  view('denuns');
+  view('users');
